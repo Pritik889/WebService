268042e8f4db4e7ff34a76a87fe2b5e7fa9c50d7
Priti Kumari <rohit.sharma@celebaltech.com>
2021-04-14 13:49:07 +0530

    webservice

---
+++ testwebserviceapp/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using myserviceclassnameSpace;
+
+public partial class _Default : System.Web.UI.Page
+{
+    /// <summary>
+    /// Here i have create the instanse of service class and
+    /// accsess method .
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        myserviceclassnameSpace.Service s = new Service();
+       string sd= s.MyServiceMethod();
+       Response.Write(sd);
+    }
+}
